5652efb8c14eb7dcfd4b59007b4a185df169e669
Volodymyr Ionaytis <blessfire91@gmail.com>
2023-07-27 20:49:14 +0200

    task01done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', handleBtnStart);
+btnStop.addEventListener('click', handleBtnStop);
+
+let timeOutId = null;
+
+function handleBtnStart() {
+    timeOutId = setInterval(() => {
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function handleBtnStop() {
+    clearInterval(timeOutId);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
